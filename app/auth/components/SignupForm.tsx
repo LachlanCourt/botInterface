95e24907122792ce4c721a579ead2fc4e2bbf889
LachlanCourt <lachlan.m.court@gmail.com>
2022-04-03 22:56:21 +1000

    Fix signup form for prepush

--- app/auth/components/SignupForm.tsx
+++ app/auth/components/SignupForm.tsx
@@ -1,5 +1,5 @@
 import { useMutation } from "blitz"
-import { LabeledTextField } from "app/core/components/LabeledTextField"
+import { Input } from "DesignSystem/components"
 import { Form, FORM_ERROR } from "app/core/components/Form"
 import signup from "app/auth/mutations/signup"
 import { Signup } from "app/auth/validations"
@@ -33,8 +33,8 @@ export const SignupForm = (props: SignupFormProps) => {
           }
         }}
       >
-        <LabeledTextField name="email" label="Email" placeholder="Email" />
-        <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
+        <Input name="email" label="Email" placeholder="Email" />
+        <Input name="password" label="Password" placeholder="Password" type="password" />
       </Form>
     </div>
   )
