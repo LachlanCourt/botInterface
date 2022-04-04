6aa1009863e2401610b2345f74cb8767cd37e088
LachlanCourt <80299053+LachlanCourt@users.noreply.github.com>
2022-04-04 22:13:04 +1000

    Only import dev dependency if not in production (#7)
    * Only import dev dependency if not in production
    
    * Update email body

--- mailers/forgotPasswordMailer.ts
+++ mailers/forgotPasswordMailer.ts
@@ -1,11 +1,9 @@
-/* TODO - You need to add a mailer integration in `integrations/` and import here.
- *
- * The integration file can be very simple. Instantiate the email client
- * and then export it. That way you can import here and anywhere else
- * and use it straight away.
- */
-import previewEmail from "preview-email"
 import mailer from "integrations/mailer"
+
+let previewEmail
+if (process.env.NODE_ENV !== "production") {
+  previewEmail = require("preview-email")
+}
 
 type ResetPasswordMailer = {
   to: string
@@ -22,7 +20,6 @@ export function forgotPasswordMailer({ to, token }: ResetPasswordMailer) {
     subject: "Your Password Reset Instructions",
     html: `
       <h1>Reset Your Password</h1>
-      <h3>NOTE: You must set up a production email integration in mailers/forgotPasswordMailer.ts</h3>
 
       <a href="${resetUrl}">
         Click here to set a new password
